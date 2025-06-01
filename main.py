c766e6c35b546b5f32ecfdcdf145056285141d29
Galandel <17754014+Galandel@users.noreply.github.com>
2025-06-01 15:29:54 -0600

    Added Player Collisions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 
 def main():
@@ -30,6 +31,11 @@ def main():
         
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
